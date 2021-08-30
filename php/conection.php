08e1e28becf83269f0fa2828c9cda0e682678b6d
ddanielsantos <contatodanieljob@gmail.com>
2021-08-30 11:47:21 -0300

    dsn heroku

--- php/conection.php
+++ php/conection.php
@@ -5,7 +5,17 @@ session_start();
 
 try {
   # estabelecendo a conexão com o banco de dados
-  $conexao = new PDO("pgsql:host=50.17.255.244;dbname=d997vi5td051t6","rfobtcvtichaaw","ba92a9958dbc4681adbc369c57bc712edef620b9eb96959368594a8927aa6ba0");
+  $db = parse_url(getenv("DATABASE_URL"));
+
+  $conexao = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+  // $conexao = new PDO("pgsql:host=50.17.255.244;dbname=d997vi5td051t6","rfobtcvtichaaw","ba92a9958dbc4681adbc369c57bc712edef620b9eb96959368594a8927aa6ba0");
 
   # Tabela de usuários
   $tbusuarios = '
